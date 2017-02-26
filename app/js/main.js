130a75395d120b6fcb1d91c871ebf26f29850440
Pavel Ivonin <pavelivonin@yandex.ru>
2017-02-26 21:16:42 +0200

    Добавил футер

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 console.log('123');
 /*Анимация блоков во время скролла*/
 window.sr = ScrollReveal();
-sr.reveal('.item', { duration: 1000 }, 500);
+sr.reveal('.item', { duration: 500 }, 500);
+
+/*Submit*/
+function CheckEmail(){
+	if (!$('#email').val()){
+		$('#email').attr('placeholder','Введите email');
+	}else{
+		$('#form_1').submit();
+		alert('Спасибо за подписку!');
+	}
+}
+$('.footer-button').on('click', CheckEmail);
